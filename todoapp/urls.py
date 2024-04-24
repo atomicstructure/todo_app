a350eb2cc253bcbd61250385838c1f7d0345cec7
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-24 14:30:38 +0100

    Created a view function for adding a Task, created the url

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,4 +3,5 @@ from todoapp import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add-task/', views.add_task, name='add_task'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -7,3 +7,9 @@ def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-created_at')
     context = {'tasks': tasks}
     return render(request, 'home.html', context)
+
+def add_task(request):
+    if request.method == 'POST':
+        task = request.POST['task']
+        Task.objects.create(task=task)
+    return render(request, 'add_task.html')
