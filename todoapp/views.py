fa91466013693888693ee1b6831069e804f50c89
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-24 16:04:29 +0100

    Added the Completed Task function in django template

--- todoapp/views.py
+++ todoapp/views.py
@@ -5,7 +5,9 @@ from .models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
-    context = {'tasks': tasks}
+
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    context = {'tasks': tasks, 'completed_tasks': completed_tasks}
     return render(request, 'home.html', context)
 
 def add_task(request):
