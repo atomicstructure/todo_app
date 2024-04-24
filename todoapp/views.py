74c10091502769ade2987edebc67b2f511d0ca7e
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-24 15:55:05 +0100

    Added the add task function

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 # Create your views here.
 
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False).order_by('-created_at')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
     context = {'tasks': tasks}
     return render(request, 'home.html', context)
 
 def add_task(request):
-    if request.method == 'POST':
         task = request.POST['task']
         Task.objects.create(task=task)
-    return render(request, 'add_task.html')
+        return redirect('home')
