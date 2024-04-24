eb055ee3a0c1e8c13affd9428730242e7580b249
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-24 13:12:53 +0100

    created models for the Todo App

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class TodoItem(models.Model):
-    content = models.TextField()
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_done = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.content
+
+    def __str__(self) -> str:
+        return self.task
